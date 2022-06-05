6c70da90cbac5d37fcf37a8b7468bf9d5ee08213
lukew3 <lukew25073@gmail.com>
2022-06-04 22:46:14 -0400

    add user to database after connection

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 import json
+import requests
+import datetime
 
 with open('config.json', 'r') as config_file:
     cfg = json.load(config_file)
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = cfg['SQLALCHEMY_DATABASE_URI']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String)
+    lastname = db.Column(db.String)
+    cur_streak = db.Column(db.Integer, default=0)
+    last_updated = db.Column(db.DateTime, default=db.func.now())
+    # Strava data
+    strava_id = db.Column(db.Integer, nullable=False, unique=True)
+    refresh_token = db.Column(db.String)
+    access_token = db.Column(db.String)
+    access_token_exp_date = db.Column(db.DateTime)
+
+db.create_all()
 
 def get_oauth_url():
     return f"http://www.strava.com/oauth/authorize?client_id={cfg['CLIENT_ID']}&response_type=code&redirect_uri=https://{cfg['DOMAIN']}/exchange_token&approval_prompt=force&scope=read"
@@ -18,6 +38,24 @@ def exchange_token():
     state = request.args.get('state')
     code = request.args.get('code')
     scope = request.args.get('scope')
+    # Get user tokens and setup user
+    res = requests.post('https://www.strava.com/oauth/token', params={
+            'client_id': cfg['CLIENT_ID'],
+            'client_secret': cfg['CLIENT_SECRET'],
+            'code': code,
+            'grant_type': 'authorization_code'
+        })
+    user_data = json.loads(res.content)
+    if User.query.filter_by(strava_id=user_data['athlete']['id']).first(): return '<p>User already exists</p>'
+    new_user = User(firstname=user_data['athlete']['firstname'],
+            lastname=user_data['athlete']['lastname'],
+            strava_id=user_data['athlete']['id'],
+            refresh_token=user_data['refresh_token'],
+            access_token=user_data['access_token'],
+            access_token_exp_date=datetime.datetime.fromtimestamp(user_data['expires_at'])
+        )
+    db.session.add(new_user)
+    db.session.commit()
     return '<p>Account linked</p>'
 
 if __name__ == '__main__':
